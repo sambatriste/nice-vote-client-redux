c142dac37f6a6b9f993501b2b7fdf60ef4c08791
Tsuyoshi Kawasaki <sambatriste@gmail.com>
2018-05-27 18:19:14 +0900

    コンポーネントをfunctionalにする。

--- src/components/Opinion.js
+++ src/components/Opinion.js
@@ -2,27 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { vote } from '../actions';
 
-class OpinionComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    const { opinion, themeId, doVote } = this.props;
-    this.onVoteClick = () => {
-      doVote(themeId, opinion.opinionId);
-    };
-  }
-
-  render() {
-    const { opinion } = this.props;
-    return (
-      <li>
-        {opinion.description}
-        <button onClick={this.onVoteClick}>
-          {opinion.agreementCount}
-        </button>
-      </li>
-    );
-  }
-}
+const OpinionComponent = ({ opinion, themeId, doVote }) => {
+  const onClick = () => { doVote(themeId, opinion.opinionId); };
+  return (
+    <li>
+      {opinion.description}
+      <button onClick={onClick}>
+        {opinion.agreementCount}
+      </button>
+    </li>
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   doVote: (themeId, opinionId) => {
